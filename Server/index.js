d224be6e514606940a5f2db624cc3d84282b1f3e
Ezer Abed <146182087+EzerAbed@users.noreply.github.com>
2024-05-23 20:51:13 +0100

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -5,7 +5,8 @@ const mongoose = require("mongoose")
 const app = express();
 
 //Importing Routes
-const signUpRouter = require('./routes/SignUp') 
+const signUpRoute = require('./routes/SignUp')
+const logInRoute = require('./routes/logIn') 
 
 //Middelware 
 app.use(express.json())
@@ -13,7 +14,9 @@ app.use(morgan("dev"))
 app.use(cors("*"))
 
 //Route 
-app.use("/signUp", signUpRouter)
+app.use("/signUp", signUpRoute)
+app.use("/logIn", logInRoute)
+
 
 //Database connection
 connectDb = async () => {
@@ -24,11 +27,11 @@ connectDb = async () => {
       })
       console.log("db connected")
     } catch (error) {
-      console.log(error.message)
+      console.log("Failled connection" + error.message)
     }
 }
 
 app.listen(8000, () => {
     connectDb()
-    console.log("listening on port 8000 !")
-})
+    console.log("listening on port 8000 !");
+});
