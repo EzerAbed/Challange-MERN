d11eb57b3c2650bc88aad2f4affffe79a506ff2e
Ezer Abed <146182087+EzerAbed@users.noreply.github.com>
2024-05-23 21:27:20 +0100

    Update SignUp.js

--- Server/controllers/SignUp.js
+++ Server/controllers/SignUp.js
@@ -1,14 +1,20 @@
 const userSchema = require('../models/SignUp')
+const signUpvalidation = require('../validation/SignUp')
 
 //Creating a new user
 const createNewUser = async (req, res) =>{
+    //verify that the req is as wanted
+    let { error, value } = signUpvalidation(req.body)
+    if (error){
+        return res.status(400).json({ message : error.details[0].message })
+    }
     try{
         const newUser = await userSchema.create(req.body)
         res.status(201).json(newUser)
-    } catch (error) {
-        if(error.code == 11000 && error.keyPattern.username){
+    } catch (e) {
+        if(e.code == 11000 && e.keyPattern.username){
             res.status(400).json({message : "The Username Already exists !"})
-        } else if (error.code == 11000 && error.keyPattern.email){
+        } else if (e.code == 11000 && e.keyPattern.email){
             res.status(400).json({message : "This Email has already been used !"})
         } else {
             res.status(500).json({message : "Unexpected error cought during the creation"})
@@ -16,7 +22,7 @@ const createNewUser = async (req, res) =>{
     }
 }
 
-//exporting all functions 
+//exporting all controllers 
 module.exports = {
     createNewUser,
 }
