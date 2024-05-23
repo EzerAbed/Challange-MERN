0b4513037592f6f1bc525cac98b5f472fc9b563a
Ezer Abed <146182087+EzerAbed@users.noreply.github.com>
2024-05-23 12:53:15 +0100

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import LogIn from './pages/LogIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <div>Home Page here</div>
+  },
+  {
+    path: "/logIn",
+    element: <LogIn />,
+  },
+  {
+    path : "/signUp",
+    element : <SignUp />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 )
