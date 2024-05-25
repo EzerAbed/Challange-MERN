2b6448550dd4d283c69b52dc4db506aaa02d5f76
Ezer Abed <146182087+EzerAbed@users.noreply.github.com>
2024-05-25 13:16:03 +0100

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
 import LogIn from './pages/LogIn.jsx';
 import SignUp from './pages/SignUp.jsx';
+import ProductsDetails from './pages/ProductsDetails.jsx'
+import Editprofile from './pages/Editprofile.jsx'
 
 const router = createBrowserRouter([
   {
@@ -19,6 +21,14 @@ const router = createBrowserRouter([
     path : "/signUp",
     element : <SignUp />,
   },
+  {
+    path : "/products/details/:id",
+    element : < ProductsDetails />
+  },
+  {
+    path : "/editAccount/:id",
+    element : < Editprofile />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
