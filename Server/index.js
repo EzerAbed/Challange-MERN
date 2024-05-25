cd23ad73197ae77c8dffc3b2da6775ad3622d329
Ezer Abed <146182087+EzerAbed@users.noreply.github.com>
2024-05-25 13:21:52 +0100

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,27 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
 const mongoose = require("mongoose")
 const app = express();
 
 //Importing Routes
 const signUpRoute = require('./routes/SignUp')
-const logInRoute = require('./routes/logIn') 
+const logInRoute = require('./routes/logIn')
+const productsDetailsRoute = require('./routes/ProductsDetails') 
+const editRouter = require("./routes/editProfile")
+const productsRoute = require('./routes/Products')
 
 //Middelware 
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(cors("*"))
 
-//Route 
+//Routes definition 
 app.use("/signUp", signUpRoute)
 app.use("/logIn", logInRoute)
+app.use("/products", productsRoute)
+app.use("/products/detail", productsDetailsRoute)
+app.use("/edit", editRouter)
 
 
 //Database connection
