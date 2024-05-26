9413622a5a53e3746d5a7aa49087d29f72bf256d
Ezer Abed <146182087+EzerAbed@users.noreply.github.com>
2024-05-26 22:33:40 +0100

    Update ProductList.jsx

--- Client/src/Pages/ProductList.jsx
+++ Client/src/Pages/ProductList.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import ProductCart from './ProductCart';
 import Sidebar from '../components/SideBar';
 import "../CSS/ProductList.css";
+import SearchBar from '../components/SearchBar';
 
 function ProductList() {
   const [products, setProducts] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   const categories = ['Apiderm', 'Express Lifting', 'Watermelon Refreshment', 'Rosacea-Stop', 'Ceramide Power'];
 
@@ -25,12 +27,24 @@ function ProductList() {
   };
 
   return (
-    <div className="product-list-page">
+    <div >
+      <div className='searchbar-container'>
+      <SearchBar setProducts={setProducts} className="searchbar"/>
+      </div>
+      <div className="product-list-page">
       <Sidebar categories={categories} onSelectCategory={handleSelectCategory} selectedCategory={selectedCategory} />
-      <div className="product-list">
-        {products.map(product => (
-          <ProductCart key={product._id} product={product} />
-        ))}
+        
+        <div className="product-list">
+          {searchResults.length > 0 ? (
+            searchResults.map(product => (
+              <ProductCart key={product._id} product={product} />
+            ))
+          ) : (
+            products.map(product => (
+              <ProductCart key={product._id} product={product} />
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
