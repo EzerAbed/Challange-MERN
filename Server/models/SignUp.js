69683a1519e52be9f874c7679763bd5b675bd42c
Ezer Abed <146182087+EzerAbed@users.noreply.github.com>
2024-05-26 16:31:09 +0100

    Update SignUp.js

--- Server/models/SignUp.js
+++ Server/models/SignUp.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 
 //definition of the Database Schema
 const userSchema = mongoose.Schema({
@@ -8,20 +9,33 @@ const userSchema = mongoose.Schema({
         unique: true,
         trim: true
     },
+
     email: {
         type: String,
         required: true,
         unique: true,
         trim: true
     },
+
     password: {
         type: String,
         required: true
     },
+    
     createdAt: {
         type: Date,
         default: Date.now
     }
+})
+
+// Hashing the password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+      return next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
 })
 
 const User = mongoose.model('User', userSchema);
