ab0417498b1ca336449a0dabb9a341af896a9d74
Ezer Abed <146182087+EzerAbed@users.noreply.github.com>
2024-05-26 12:20:53 +0100

    Update Products.js

--- Server/routes/Products.js
+++ Server/routes/Products.js
@@ -1,10 +1,12 @@
 const router = require("express").Router()
 
 //import the conrollers
-const { getAll, createNewProduct } = require('../controllers/Products')
+const { getAll, createNewProduct, getBestProducts, getNewestProudct } = require('../controllers/Products')
 
 //GET requests
 router.get("/", getAll)
+router.get("/bestProducts", getBestProducts)
+router.get("/newestProducts", getNewestProudct)
 
 //POST requests
 router.post("/", createNewProduct)
