b245e6547b6892725418445d50852564f8714798
Ezer Abed <146182087+EzerAbed@users.noreply.github.com>
2024-05-26 09:56:11 +0100

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,40 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import ContextManager from './ContextManager.jsx'
-import { createBrowserRouter, RouterProvider} from "react-router-dom";
-import LogIn from './pages/LogIn.jsx';
-import SignUp from './pages/SignUp.jsx';
-import ProductsDetails from './pages/ProductsDetails.jsx'
-import Editprofile from './pages/Editprofile.jsx'
+import LogIn from './Pages/LogIn.jsx';
+import SignUp from './Pages/SignUp.jsx';
+import ProductsDetails from './Pages/ProductsDetails.jsx'
+import Editprofile from './Pages/Editprofile.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path : "/",
-    element : <div>Home Page here</div>
-  },
-  {
-    path: "/logIn",
-    element: <LogIn />,
-  },
-  {
-    path : "/signUp",
-    element : <SignUp />,
-  },
-  {
-    path : "/products/details/:id",
-    element : < ProductsDetails />
-  },
-  {
-    path : "/editAccount/:id",
-    element : < Editprofile />
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    
     <ContextManager>
-    <RouterProvider router={router} />
-    <App />
+      <BrowserRouter >
+          <App />
+        </BrowserRouter>
     </ContextManager>
+    
   </React.StrictMode>,
 )
