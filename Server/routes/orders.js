6f2ca81ef986925d71161ffaf7e895ebfda4f748
Ezer Abed <146182087+EzerAbed@users.noreply.github.com>
2024-05-27 03:54:08 +0100

    Update orders.js

--- Server/routes/orders.js
+++ Server/routes/orders.js
@@ -1,16 +1,17 @@
 const router = require("express").Router()
 
 //Importing controllers
-const { getAllOrders, createOrder, deleteOrderById } = require("../controllers/orders")
+const { getAllOrders, createOrder, deleteOrderById, getOrderByUserId, updateStatue } = require("../controllers/orders")
 
 //GET requests
 router.get("/", getAllOrders)
+router.get("/:id", getOrderByUserId)
 
 //POST requests
 router.post("/", createOrder)
 
 //PUT requests
-
+router.put("/updateStatue", updateStatue)
 //DELETE requests
 router.delete("/:id", deleteOrderById)
 
