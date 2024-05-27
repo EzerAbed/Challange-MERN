eff131f73456a24e4be000ceed5e6e068c4be0ba
Ezer Abed <146182087+EzerAbed@users.noreply.github.com>
2024-05-27 03:52:42 +0100

    Update order.js

--- Server/models/order.js
+++ Server/models/order.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose")
+const userSchema = require("./SignUp.js")
+const productSchema = require("./Products.js")
+const { type } = require("../validation/contact.js")
 
 //definition of the orders database schema
 const ordersSchema = mongoose.Schema({
     idCustumer : {
         type : mongoose.Schema.Types.ObjectId,
-        ref : 'users',
+        ref : 'User',
         required : true
     },
 
     idProduct : {
         type : mongoose.Schema.Types.ObjectId,
-        ref: 'products', 
+        ref: 'Product', 
         required : true
     },
 
@@ -22,6 +25,11 @@ const ordersSchema = mongoose.Schema({
     price : {
         type : Number,
         required : true
+    },
+
+    status : {
+        type : Boolean,
+        default : false
     },
 
     createdAt : {
